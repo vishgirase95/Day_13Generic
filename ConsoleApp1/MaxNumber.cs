fcb1cdaf8d7b431135fbe4ee7060e0c6c336d6cc
vish <vishgirase95@gmail.com>
2021-11-02 16:00:50 +0530

    [vishal] added check max int

---
+++ ConsoleApp1/MaxNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class MaxNumber
+    {
+        
+        public static void print(int a, int b, int c)
+        {
+            if((a.CompareTo(b)>0 && a.CompareTo(c) >= 0) ||
+               (a.CompareTo(b) >= 0 && a.CompareTo(c) > 0)||
+               (a.CompareTo(b) > 0 && a.CompareTo(c) >0))
+
+
+            {
+                Console.WriteLine("Maxnumber :" +a);
+            }
+            else if((b.CompareTo(c)>0 && b.CompareTo(a) >= 0)||
+                (b.CompareTo(c) >= 0 && b.CompareTo(a) > 0)||
+                (b.CompareTo(c) > 0 && b.CompareTo(a) > 0))
+            {
+                Console.WriteLine("Maxnumber :" +b);
+            }
+            else if((c.CompareTo(a)>0 && b.CompareTo(a)>=0)||
+                (c.CompareTo(a) >= 0 && b.CompareTo(a) > 0)||
+                (c.CompareTo(a) > 0 && b.CompareTo(a) > 0)) {
+
+                Console.WriteLine("Maxnumber :" + c);
+            }
+            else
+            {
+                Console.WriteLine("All number are equal");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        
+           MaxNumber.print(10,10,10);
+          
+        }
+
+        
+    }
+}
